6f06f8c7cb0fc8e8ed8c5f27dadf9c76139d5d83
Joaquin Macias <omermacias@gmail.com>
2025-05-15 15:42:56 -0400

    Subiendo archivo de tipos de datos

---
+++ Python-2025/02.tiposdedatos.py
@@ -0,0 +1,18 @@
+# 15 DE MAYO 2025
+
+#01 DECLARANDO E INICIALIZANDO VARIABLES NÚMERICAS
+
+edad = 31
+estatura = 1.71
+peso = 75.4
+complejo = 2 + 9j # INICIALIZANDO UN NÚMERO COMPLEJO
+complejo2 = complex(1, 4) # OTRA FORMA DE INICIALIZAR UN NÚMERO COMPLEJO
+
+print(complejo)
+print(complejo2)
+
+#OPERACIÓN ARITMÉTICA
+imc = peso / estatura ** 2
+print(f"Su IMC es: {imc}")
+
+ 
