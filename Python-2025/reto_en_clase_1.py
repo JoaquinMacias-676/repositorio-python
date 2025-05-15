9eb2dfa903009e9ac66b589e7edcd165ca8e521a
Joaquin Macias <omermacias@gmail.com>
2025-05-15 16:39:43 -0400

    Primer reto en clases

---
+++ Python-2025/reto_en_clase_1.py
@@ -0,0 +1,9 @@
+# INGRESO DE VARIABLES MEDIANTE CONSOLA
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad:")
+
+# OPERACIÓN PARA SUMAR 20 AÑOS A LA EDAD INGRESADA
+total_edad = int(edad) + 20 
+
+# RESULTADO
+print(f"Hola mi nombre es {nombre}, tengo {edad} años y en 20 años tendré {total_edad} años.")
