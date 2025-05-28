cce225675516dbd2d2fcefb27a8d282158bd3adb
Joaquin Macias <omermacias@gmail.com>
2025-05-28 16:06:36 -0400

    Apuntes 1

---
+++ Python-2025/apuntes.py
@@ -0,0 +1,9 @@
+#{} LLAVES
+#() PARENTESIS
+#[] CORCHETES
+# min() Devolvería el número más pequeño entre un conjunto de números [1,2,3,4,5] = arrojaría el 1
+# round() Redondea el valor de un número específico de dígitos decimales
+
+'''Lista = []
+   Tuplas = ()
+   Sets = {}'''
