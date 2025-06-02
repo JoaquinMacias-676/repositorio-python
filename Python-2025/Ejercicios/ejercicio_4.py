6cb76a5ecb87aab5bc3e416404802e253e8497cf
Joaquin Macias <omermacias@gmail.com>
2025-06-02 13:02:57 -0400

    Subiendo ejercicio 4

---
+++ Python-2025/Ejercicios/ejercicio_4.py
@@ -0,0 +1,12 @@
+'''Desarrollar un programa de gestión de inventario:
+A. Ingresar el nombre de un producto y su precio unitario.
+B. Ingresar la cantidad en stock.
+C. Calcular el valor total de los productos ingresados y mostrarlo con 2 decimales.
+D. Crear una variable booleana llamada umbral, que entregue un True si el valor_total >
+100000 y False en caso contrario..
+E. Imprimir el nombre del producto, la cantidad, el valor total y el estado umbral en un solo
+print() formateado.'''
+
+producto_1 = input('Ingrese el nombre del producto: ')
+precio_1 = input('Ingrese el precio del producto' )
+stock = input('Ingrese la cantidad de stock de la cual se dispone: ')
