54b3e19417d03a146ca4ce979158bdb60dc402a4
Joaquin Macias <omermacias@gmail.com>
2025-06-02 10:37:18 -0400

    Segundo reto en clase

---
+++ Python-2025/reto_en_clase_2.py
@@ -0,0 +1,21 @@
+# DEFINIENDO LOS PRECIOS DE LAS FRUTAS
+precios_1 = (1000, 1500, 2000)
+precios_2 = (1750, 1750, 2250)
+precios_3 = (1900, 2100, 2500)
+
+# CREANDO DICCIONARIO (INVENTARIO)
+inventario = {
+    'Manzana': precios_1,
+    'Platano': precios_2,
+    'Cereza': precios_3
+}
+
+sorted(precios_2)
+
+# LISTA DE FRUTAS
+tipos_frutas = ['Manzana', 'Platano', 'Cereza']
+
+
+# IMPRESIÓN
+print(inventario)
+print(tipos_frutas)
