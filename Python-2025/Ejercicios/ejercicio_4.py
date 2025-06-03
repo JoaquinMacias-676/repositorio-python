c6f47786962e78f3c7cb7c83984443d6c680b10f
Joaquin Macias <omermacias@gmail.com>
2025-06-03 10:17:46 -0400

    Ejercicio 4 modificado

--- Python-2025/Ejercicios/ejercicio_4.py
+++ Python-2025/Ejercicios/ejercicio_4.py
@@ -7,11 +7,17 @@ D. Crear una variable booleana llamada umbral, que entregue un True si el valor_
 E. Imprimir el nombre del producto, la cantidad, el valor total y el estado umbral en un solo
 print() formateado.'''
 
+# INGRESO DE VALORES POR CONSOLA
 producto = input('Ingrese el nombre del producto: ')
+
 precio = int(input('Ingrese el precio del producto: ' ))
+
 stock = int(input('Ingrese la cantidad de stock de la cual se dispone: '))
 
+# CÁLCULO Y UMBRAL
 valor_total = precio * stock
+
 umbral = int(valor_total) > 100000
 
+# IMPRESIÓN
 print(f'El nombre del producto es "{producto}", con una cantidad de {stock} y dando como valor total ${valor_total:.2f} CLP y con un estado umbral = {umbral}')
