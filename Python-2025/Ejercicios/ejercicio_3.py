ed2b12f7ceb935065e2e1741b2007b5ffbd6a416
Joaquin Macias <omermacias@gmail.com>
2025-06-03 10:16:17 -0400

    Subiendo ejercicio 3

--- Python-2025/Ejercicios/ejercicio_3.py
+++ Python-2025/Ejercicios/ejercicio_3.py
@@ -5,13 +5,24 @@ C. Utilizar un método propio para determinar cuántas veces aparece la letra «
 como «A») en la frase, y muestra el total.
 D. Emplear otro método para imprimir la longitud de la frase original.'''
 
+# INGRESO DE FRASE MEDIANTE CONSOLA
 frase = str(input('Ingrese una frase, que tendrá como máximo 30 caracteres: '))[0:30]
 
+# FRASE CON VARIABLES EN MAYÚSCULAS Y MINÚSCULAS
 mayor = frase.upper()
+
 menor = frase.lower()
 
+# CONTADORES DE "a" Y "A"
 cantidad_a = frase.count('a')
+
 cantidad_A = frase.count('A')
+
+# IMPRESIÓN DE FRASES
+
+print(f'La frase con variables en mayúsculas es: {mayor}')
+
+print(f'La frase con variables en minúsculas es: {menor}')
 
 print(f'Hay un total de {cantidad_a} "a"')
 
