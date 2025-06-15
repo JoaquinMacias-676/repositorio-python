3ab06f60c18bb14fc6b18400cc30c6e7c3ad6b79
Joaquin Macias <omermacias@gmail.com>
2025-06-15 14:35:19 -0400

    Cambios en el orden de archivos

---
+++ Python-2025/Guias/ejercicio_1.py
@@ -0,0 +1,8 @@
+'''. Contar las repeticiones de una palabra en una tupla:
+“El Proyecto Integrador del primer semestre, se evaluara y desarrollará en 3 asignaturas en conjunto: Taller de Introduccion a la Ingeniería donde trabajaran el desarrollo práctico del proyecto, 
+Habilidades Comunicativas donde desarrollaran las habilidades de presentación y redacción y por último Programación, donde aplicarán técnicas para codificar y diseñar el software del proyecto.” 
+a) Lea el parrafo. Este debe ser ingresado por teclado.
+b) Separe sus palabras y debe guardarlas en una lista.
+c) Solicite al usuario una palabra a buscar.
+d) Imprima cuantas veces aparece dicha palabra (Debe ser sensible a mayúsculas y minúsculas).
+Requisito especial: Si el parrafo está vacío, debe lanzar y capturar una excepcion (ValueError) indicando “El texto no puede estar vacío".'''
