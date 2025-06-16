5ecd2ce7d4330fb2d91781e4d33b6c3dc43ae588
Joaquin Macias <omermacias@gmail.com>
2025-06-15 22:45:21 -0400

    Cambios en el ejercicio 3

--- Python-2025/Guias/guia_ejercicio3.py
+++ Python-2025/Guias/guia_ejercicio3.py
@@ -33,41 +33,43 @@ tienda = {
     'Roberto': lista_roberto
 }
 
+print('####### REPORTE: SUELDOS A PAGAR #######\n')
+
 # Impresión del sueldo de Juan
 if sum_listajuan < 500000:
-    print(f'Juan no cuenta con ningún bono, su sueldo a pagar es de: ${sueldo_base} ')
+    print(f'|Juan no cuenta con ningún bono, su sueldo a pagar es de: ${sueldo_base} CLP|')
 elif sum_listajuan >= 500000 and sum_listajuan < 1000000:
     sueldo_j5 = sueldo_base + (sueldo_base * 5/100)
-    print(f'Juan cuenta con un bono del 5%, su sueldo a pagar es de ${sueldo_j5}')
+    print(f'|Juan cuenta con un bono del 5%, su sueldo a pagar es de ${sueldo_j5} CLP|')
 elif sum_listajuan >= 1000000 and sum_listajuan < 1500000:
     sueldo_j10 = sueldo_base + (sueldo_base * 10/100)
-    print(f'Juan cuenta con un bono del 10%, su sueldo a pagar es de ${sueldo_j10}')
+    print(f'|Juan cuenta con un bono del 10%, su sueldo a pagar es de ${sueldo_j10} CLP|')
 elif sum_listajuan >= 1500000:
     sueldo_j20 = sueldo_base + (sueldo_base * 20/100)
-    print(f'Juan cuenta con un bono del 20%, su sueldo a pagar es de ${sueldo_j20}')
+    print(f'|Juan cuenta con un bono del 20%, su sueldo a pagar es de ${sueldo_j20} CLP|')
 
 # Impresión del sueldo de Sergio
 if sum_listasergio< 500000:
-    print(f'Juan no cuenta con ningún bono, su sueldo a pagar es de: ${sueldo_base} ')
+    print(f'|Sergio no cuenta con ningún bono, su sueldo a pagar es de: ${sueldo_base} CLP|')
 elif sum_listasergio >= 500000 and sum_listasergio < 1000000:
     sueldo_s5 = sueldo_base + (sueldo_base * 5/100)
-    print(f'Juan cuenta con un bono del 5%, su sueldo a pagar es de ${sueldo_s5}')
+    print(f'|Sergio cuenta con un bono del 5%, su sueldo a pagar es de ${sueldo_s5} CLP|')
 elif sum_listasergio >= 1000000 and sum_listasergio < 1500000:
     sueldo_s10 = sueldo_base + (sueldo_base * 10/100)
-    print(f'Juan cuenta con un bono del 10%, su sueldo a pagar es de ${sueldo_s10}')
+    print(f'|Sergio cuenta con un bono del 10%, su sueldo a pagar es de ${sueldo_s10} CLP|')
 elif sum_listasergio >= 1500000:
     sueldo_s20 = sueldo_base + (sueldo_base * 20/100)
-    print(f'Juan cuenta con un bono del 20%, su sueldo a pagar es de ${sueldo_s20}')
+    print(f'|Sergio cuenta con un bono del 20%, su sueldo a pagar es de ${sueldo_s20} CLP|')
 
 # Impresión del sueldo de Roberto
 if sum_listaroberto< 500000:
-    print(f'Juan no cuenta con ningún bono, su sueldo a pagar es de: ${sueldo_base} ')
+    print(f'|Roberto no cuenta con ningún bono, su sueldo a pagar es de: ${sueldo_base} CLP|')
 elif sum_listaroberto >= 500000 and sum_listaroberto < 1000000:
     sueldo_r5 = sueldo_base + (sueldo_base * 5/100)
-    print(f'Juan cuenta con un bono del 5%, su sueldo a pagar es de ${sueldo_r5}')
+    print(f'|Roberto cuenta con un bono del 5%, su sueldo a pagar es de ${sueldo_r5} CLP|')
 elif sum_listaroberto >= 1000000 and sum_listaroberto < 1500000:
     sueldo_r10 = sueldo_base + (sueldo_base * 10/100)
-    print(f'Juan cuenta con un bono del 10%, su sueldo a pagar es de ${sueldo_r10}')
+    print(f'|Roberto cuenta con un bono del 10%, su sueldo a pagar es de ${sueldo_r10} CLP|')
 elif sum_listaroberto >= 1500000:
     sueldo_r20 = sueldo_base + (sueldo_base * 20/100)
-    print(f'Juan cuenta con un bono del 20%, su sueldo a pagar es de ${sueldo_r20}')
+    print(f'|Roberto cuenta con un bono del 20%, su sueldo a pagar es de ${sueldo_r20} CLP|')
