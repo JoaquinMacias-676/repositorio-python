2137fdc1d93e1d78d47dcafd3144c6bb9056b49c
Joaquin Macias <omermacias@gmail.com>
2025-06-17 00:30:07 -0400

    Subiendo el reto 3 hecho en clase

---
+++ Python-2025/Retos en clase/reto_en_clase_3.py
@@ -0,0 +1,20 @@
+# Inicializando la suma en 0
+sum = 0
+
+# Inicializando a en 500
+a = 500
+
+print('Los números del 500 al 100, son: ')
+
+# Sumatoria del 500 al 100, decreciendo en 3
+while a >= 100:
+    print(a)
+    a -= 3 
+    sum += a + a
+
+# Sumada de todos los números
+print(f'La suma de todos los números da como resultado: {sum}')
+
+lista = [a]
+
+print(f'La cantidad de numeros que se suman es: {lista}')
