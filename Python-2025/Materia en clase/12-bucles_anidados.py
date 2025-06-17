f09ed7bfb9a6c71519fad8e0cd014dad9082632f
Joaquin Macias <omermacias@gmail.com>
2025-06-17 00:30:41 -0400

    Subiendo materia de bucles anidados

---
+++ Python-2025/Materia en clase/12-bucles_anidados.py
@@ -0,0 +1,7 @@
+# Ejemplo: Búsquedad de un número primo
+
+for n in range(2, 11):
+    for i in range(2, n):
+        if n % i == 0:
+            print(f'{n} es un número compuesto, divisible por {i}')
+            break
