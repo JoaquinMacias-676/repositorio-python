f2a9337c0629ba075895971172f1a7c95caa826f
Joaquin Macias <omermacias@gmail.com>
2025-06-22 17:46:44 -0400

    mejora del ejercicio 2 del lab 2

--- Python-2025/Labs/Lab 2/ejercicio2_laboratorio2.py
+++ Python-2025/Labs/Lab 2/ejercicio2_laboratorio2.py
@@ -1,7 +1,7 @@
 # JOAQUÍN MACÍAS Y DAYANA LEVICOY
 
 # RESUMEN INGRESADO MEDIANTE CONSOLA
-resumen = str(input('Ingrese un resumen: '))
+resumen = str(input('\nIngrese un resumen: '))
 
 # VERIFICACIÓN DE RESUMEN (SI CUMPLE CON EL LÍMITE DE 50)
 boleano_resumen = len(resumen) <= 50
@@ -9,22 +9,25 @@ boleano_resumen = len(resumen) <= 50
 # CONTADOR DE VECES QUE APARECE "e"
 num_e = resumen.count('e')
 
+# RESUMEN CON COMAS
+comas = resumen.split()
+
 # IMPRESIÓN DE SI EL RESUMEN CUMPLE CON LOS LÍMITES
-print(f'¿El resumen cumple con el límite?: {boleano_resumen}')
+print(f'\n¿El resumen cumple con el límite?: {boleano_resumen}')
 
 # IMPRESIÓN DE LONGITUD DEL RESUMEN
-print(f'La longitud del resumen es de: {len(resumen)} caracteres')
+print(f'\nLa longitud del resumen es de: {len(resumen)} caracteres')
 
 # IMPRESIÓN DE RESUMEN EN MAYÚSCULAS
-print(f'Resumen en mayúsculas: {resumen.upper()}')
+print(f'\nResumen en mayúsculas:\n{resumen.upper()}\n')
 
 # IMPRESIÓN DE RESUMEN EN MINÚSCULAS
-print(f'Resumen en minúsculas: {resumen.lower()}')
+print(f'Resumen en minúsculas:\n{resumen.lower()}\n')
 
 # IMPRESIÓN DE VECES QUE APARECE LA VOCAL "e"
-print(f'Número de veces que aparece la vocal e {num_e}')
+print(f'Número de veces que aparece la vocal e: {num_e}')
 
 # INTENTO DE IMPRESIÓN
-print(f'Mostrar los primeros 15 caracteres {resumen[14]}')
-#print(f'Mostrar los últimos 15 caracteres {}')
-#print(f'Resumen con sus palabras unidas con coma {resumen.}')
+print(f'\nMostrar los primeros 15 caracteres: {resumen[:15]}')
+print(f'\nMostrar los últimos 15 caracteres: {resumen[-15:]}')
+print(f'\nResumen con sus palabras unidas con coma: {comas}\n')
