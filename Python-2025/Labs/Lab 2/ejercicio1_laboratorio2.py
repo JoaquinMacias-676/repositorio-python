33e07153a21f9fb5bb402d588d3b75a03296ca8d
Joaquin Macias <omermacias@gmail.com>
2025-06-22 17:51:30 -0400

    Cambios pequeños en el ejercicio 1 de lab 2

--- Python-2025/Labs/Lab 2/ejercicio1_laboratorio2.py
+++ Python-2025/Labs/Lab 2/ejercicio1_laboratorio2.py
@@ -1,7 +1,7 @@
 # INTEGRANTES: JOAQUÍN MACÍAS, DAYANA LEVICOY
 
 # INGRESO DE PRECIOS Y CANTIDADES MEDIANTE CONSOLAS (MARTILLO)
-precio_m = float(input('Ingrese el valor del Martillo: '))
+precio_m = float(input('\nIngrese el valor del Martillo: '))
 cantidad_m = int(input('Ingrese la cantidad que hay del producto "Martillo": '))
 
 # INGRESO DE PRECIOS Y CANTIDADES MEDIANTE CONSOLAS (CLAVOS)
@@ -38,7 +38,7 @@ iva_subtotales = sum_totales + (sum_totales * 19/100)
 prom_unitario = (precio_m + precio_c + precio_s + precio_t)/4
 
 # IMPRESIÓN SUBTOTALES
-print(f'Subtotal Martillo: ${round(subtotal_m, 2)} CLP | Subtotal Clavos: ${round(subtotal_c, 2)} CLP | Subtotal Serrucho: ${round(subtotal_s, 2)} CLP | Subtotal Tornillos: ${round(subtotal_t, 2)} CLP')
+print(f'\nSubtotal Martillo: ${round(subtotal_m, 2)} CLP | Subtotal Clavos: ${round(subtotal_c, 2)} CLP | Subtotal Serrucho: ${round(subtotal_s, 2)} CLP | Subtotal Tornillos: ${round(subtotal_t, 2)} CLP')
 
 # IMPRESIÓN DE VALOR TOTAL DE SUBTOTALES
 print(f'Valor total de los Subtotales: ${sum_totales} CLP')
@@ -51,4 +51,4 @@ print(f'El minimo entre los 4 subtotales es de: ${min(subtotal_m, subtotal_c, su
 # IMPRESIÓN DE PROMEDIO SUBTOTALES
 print(f'El promedio de los Subtotales: ${round(prom_unitario, 2)} CLP')
 
-print(f'Valor total de los Subtotales con el IVA (19%) aplicado: ${iva_subtotales} CLP')
+print(f'Valor total de los Subtotales con el IVA (19%) aplicado: ${iva_subtotales} CLP\n')
