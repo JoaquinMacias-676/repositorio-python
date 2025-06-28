fd95ee7bff9fc380ea0c0d1f066ffb1e586f97d4
Joaquin Macias <omermacias@gmail.com>
2025-06-28 00:13:14 -0400

    Subiendo la materia vista en clase sobre Colaboratory

--- Python-2025/Apuntes/apuntes.py
+++ Python-2025/Apuntes/apuntes.py
@@ -10,3 +10,11 @@
 
 # == no es lo mismo que = | El primero es para hacer una igualdad, mientras que el segundo es para dar un valor a algo
 # != : no es igual
+
+# def sirve para encapsular código, y nos permite llamarlo desde cualquier punto del código,
+# usandose para no tener que repetirlo varias veces en el código, siendo más ordenado
+
+# dtype: sirve para consultar el tipo de dato número de un arreglo (solo disponible con numpy)
+
+''' MATERIA/INFORMACIÓN '''
+# panda cubre lectura de archivos, se puede trabajar con archivos excel, SQL, machine learning, etc.
